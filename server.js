f0534fd1c992e8d53cfe7b850eb862705684f37e
rashmipullur <mheresince1998@gmail.com>
2024-01-12 23:29:21 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config.env' });
+const app = require('./app');
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+mongoose
+  // .connect(process.env.DATABASE_LOCAL, {
+  .connect(DB, {
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
+    // useCreateIndex: true,
+    // useFindAndModify: false,
+  })
+  .then(() => console.log('DB connection successful.'));
+
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+  console.log(`app running on port ${port}...`);
+});
