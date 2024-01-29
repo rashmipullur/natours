1fe17be6315fa07fd9bc2bd41ea7cf107735df74
rashmipullur <mheresince1998@gmail.com>
2024-01-29 20:59:02 +0530

    building handler factory functions: delete

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,6 +2,7 @@ const Tour = require('../models/tourModel');
 const APIFeatures = require('../utils/apiFeatures');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
+const factory = require('./handlerFactory');
 // const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`));
 
 exports.aliasTopTours = (req, res, next) => {
@@ -58,15 +59,17 @@ exports.updateTour = catchAsync(async (req, res, next) => {
   res.status(200).json({ status: 'success', data: { tour } });
 });
 
-exports.deleteTour = catchAsync(async (req, res, next) => {
-  const tour = await Tour.findByIdAndDelete(req.params.id);
+exports.deleteTour = factory.deleteOne(Tour);
 
-  if (!tour) {
-    return next(new AppError('no tour found with that ID', 404));
-  }
+// exports.deleteTour = catchAsync(async (req, res, next) => {
+//   const tour = await Tour.findByIdAndDelete(req.params.id);
 
-  res.status(204).json({ status: 'success', data: null });
-});
+//   if (!tour) {
+//     return next(new AppError('no tour found with that ID', 404));
+//   }
+
+//   res.status(204).json({ status: 'success', data: null });
+// });
 
 exports.getTourStats = catchAsync(async (req, res, next) => {
   const stats = await Tour.aggregate([
