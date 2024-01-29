1088fe3d51696879b5737828414e1072bef90317
rashmipullur <mheresince1998@gmail.com>
2024-01-30 00:47:00 +0530

    factory functions: update & create ✌

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -11,3 +11,30 @@ exports.deleteOne = (Model) =>
 
     res.status(204).json({ status: 'success', data: null });
   });
+
+exports.updateOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+
+    const modelName = Model.modelName.toLowerCase();
+
+    if (!doc) {
+      return next(new AppError(`no ${modelName} found with that ID`, 404));
+    }
+
+    const data = {};
+    data[modelName] = doc;
+
+    res.status(200).json({ status: 'success', data });
+  });
+
+exports.createOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.create(req.body);
+    const data = {};
+    data[Model.modelName.toLowerCase()] = doc;
+    res.status(201).json({
+      status: 'success',
+      data,
+    });
+  });
--- controllers/userController.js
+++ controllers/userController.js
@@ -60,12 +60,6 @@ exports.createUser = (req, res) => {
     message: 'this route is not yet defined',
   });
 };
-
-exports.updateUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'this route is not yet defined',
-  });
-};
-
+// do not update passwords with this!
+exports.updateUser = factory.updateOne(User);
 exports.deleteUser = factory.deleteOne(User);
