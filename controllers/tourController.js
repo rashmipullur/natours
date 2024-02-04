ffca57503ebb73ddf71381dd55565fd496e7f4e0
rashmipullur <mheresince1998@gmail.com>
2024-02-04 20:18:31 +0530

    geospatial aggregation: calc distances

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -89,7 +89,6 @@ exports.getMonthlyPlan = catchAsync(async (req, res, next) => {
 
 exports.getToursWithin = catchAsync(async (req, res, next) => {
   const { distance, latlng, unit } = req.params;
-  // 34.114461,-118.318408
   const [lat, lng] = latlng.split(',');
 
   const radius = unit === 'mi' ? distance / 3963.2 : distance / 6378.1;
@@ -104,5 +103,40 @@ exports.getToursWithin = catchAsync(async (req, res, next) => {
     status: 'success',
     results: tours.length,
     data: { data: tours },
+  });
+});
+
+exports.getDistances = catchAsync(async (req, res, next) => {
+  const { latlng, unit } = req.params;
+  const [lat, lng] = latlng.split(',');
+
+  const multiplier = unit === 'mi' ? 0.000621371 : 0.001;
+
+  if (!lat || !lng) {
+    next(new AppError('please provide latitude and longitude in the format lat,lng', 400));
+  }
+
+  const distances = await Tour.aggregate([
+    {
+      $geoNear: {
+        near: {
+          type: 'Point',
+          coordinates: [lng * 1, lat * 1],
+        },
+        distanceField: 'distance',
+        distanceMultiplier: multiplier,
+      },
+    },
+    {
+      $project: {
+        distance: 1,
+        name: 1,
+      },
+    },
+  ]);
+
+  res.status(200).json({
+    status: 'success',
+    data: { data: distances },
   });
 });
