8dc371f71acffa867c9f27e583c79376896a3f72
rashmipullur <mheresince1998@gmail.com>
2024-02-08 23:44:13 +0530

    included a map with mapbox

--- app.js
+++ app.js
@@ -24,7 +24,12 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // set security HTTP headers
-app.use(helmet());
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+    crossOriginEmbedderPolicy: false,
+  }),
+);
 
 // development logging
 if (process.env.NODE_ENV === 'development') {
