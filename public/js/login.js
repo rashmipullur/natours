3a920d515f65c604a09143fdfee32aa3fa09caa9
rashmipullur <mheresince1998@gmail.com>
2024-02-13 23:22:16 +0530

    login correction

--- public/js/login.js
+++ public/js/login.js
@@ -5,13 +5,13 @@ const login = async (email, password) => {
   try {
     const res = await axios({
       method: 'POST',
-      url: 'http://127.0.0.1:3000/api/v1/users/login',
+      url: 'http://localhost:3000/api/v1/users/login',
       data: {
         email,
         password,
       },
     });
-    console.log('-->', res);
+    console.log(res);
   } catch (error) {
     console.log(error.response.data);
   }
@@ -20,6 +20,6 @@ const login = async (email, password) => {
 document.querySelector('.form').addEventListener('submit', (e) => {
   e.preventDefault();
   const email = document.getElementById('email').value;
-  const password = document.getElementById('email').value;
-  login({ email, password });
+  const password = document.getElementById('password').value;
+  login(email, password);
 });
