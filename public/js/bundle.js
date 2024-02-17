a58644a1d8356ac93c3cf4aa4ecd7c1de69135da
rashmipullur <mheresince1998@gmail.com>
2024-02-17 22:18:49 +0530

    updating user data

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -12250,7 +12250,7 @@ var _login = require("./login");
 
 // DOM ELEMENTS
 var mapBox = document.getElementById('map');
-var loginForm = document.querySelector('.form');
+var loginForm = document.querySelector('.form--login');
 var logOutBtn = document.querySelector('.nav__el--logout');
 
 // DELEGATION
@@ -12290,7 +12290,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51486" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53365" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
